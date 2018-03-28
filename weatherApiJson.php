ecac5745d04c421c9e04f9f4eb1613806bfa9cc9
Sweetpurple616 <zoedewaard@hotmail.nl>
2018-03-28 16:38:48 +0200

    added image

--- weatherApiJson.php
+++ weatherApiJson.php
@@ -7,17 +7,23 @@ $json = file_get_contents($website);
 $decodedJson = json_decode($json);
 $temp = $decodedJson->list[0]->main->temp;
 $weather = $decodedJson->list[0]->weather[0]->main;
+$image = $decodedJson->list[0]->weather[0]->icon;
 $tempOver3 = $decodedJson->list[1]->main->temp;
 $weatherOver3 = $decodedJson->list[1]->weather[0]->main;
+$weatherimg = "http://openweathermap.org/img/w/" . $image. ".png";
 
 $weer = [
     "temp" => $temp,
     "weather" => $weather,
+    "iconurl" => $weatherimg,
     "tempOver3" => $tempOver3,
     "weatherOver3" => $weatherOver3
 ];
+
+// add image
 
 echo json_encode($weer);
 header("Content-type:application/json");
 
 ?>
+
